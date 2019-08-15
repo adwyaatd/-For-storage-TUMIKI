c6895594104210b81a54f545b770e49025a69f87
細田来夢 <>
2019-08-15 16:36:38 +0900

    最初のコミットです。

---
+++ TUMIKI_app/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'users/index'
+
+  get "/" => "home#top"
+  get "/about" => "home#about"
+  get "/posts/index" => "posts#index"
+  get "/posts/new" => "posts#new"
+  post "/posts/create" => "posts#create"
+  get "/posts/:id" => "posts#show"
+  get "/posts/:id/edit" => "posts#edit"
+ post "/posts/:id/update" => "posts#update"
+ post "/posts/:id/destroy" => "posts#destroy"
+
+ get "/users/index" => "users#index"
+ get "/users/:id" => "users#show"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
